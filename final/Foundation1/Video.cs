a15ac318aac299a125f12b49547e5a262c781d78
constence <hol20087@byui.edu>
2025-07-08 16:11:51 -0600

    week_one

---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+public class Video
+{
+    public string _title; 
+    public string _author; 
+    public int _seconds; 
+    private List<Comment> _comments = new List<Comment>();
+
+    public Video(string title, string author, int seconds)
+    { 
+        _title = title; 
+        _author = author; 
+        _seconds = seconds; 
+        _comments = new List<Comment>();
+    }
+
+    public void AddComment(string name, string text)
+    {
+        _comments.Add(new Comment(name, text));
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+    public int GetMin()
+    {
+        int min = _seconds/60; 
+        return min; 
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {_title}\nAuthor: {_author}\nLength in seconds: {_seconds} seconds\n Length in Minutes:{GetMin()} Minutes\n Comments: {GetCommentCount()}");
+        Console.WriteLine("Comments:");
+        foreach (Comment comment in _comments)
+        {
+            Console.WriteLine($"- {comment.GetName()}: {comment.GetText()}");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,57 @@
+class Order
+{
+    private List<Product> _productsList;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _productsList = new List<Product>();
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _productsList.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double totalCost = 0;
+        foreach ( Product product in _productsList)
+        { 
+            totalCost += product.GetTotalCost();
+        }
+        
+        double shippingCost = 0; 
+        if (_customer.IsInUSA())
+        {
+            shippingCost = 5;
+        }
+        else
+        {
+            shippingCost = 35;
+        }
+        
+        return totalCost + shippingCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "";
+        foreach (Product product in _productsList)
+        {
+            label += $"{product.GetName()} (ID: {product.GetProductId()})";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+
+    public DateTime Arrival (int daysToShip)
+    {
+        return DateTime.Today.AddDays(daysToShip);
+    }
+}
